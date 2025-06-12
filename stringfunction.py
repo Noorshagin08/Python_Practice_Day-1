3e3d6c8991fed85e6bfe5d12b714c0c0df62ecf9
Noorshagin08 <noorshagin05@gmail.com>
2025-06-12 20:44:36 +0530

    String Function

---
+++ stringfunction.py
@@ -0,0 +1,23 @@
+# String Functions
+
+a="ram kumar "
+b="ram,kumar"
+c="ram kumar \nraj kumar"
+d="    ram"
+
+print(len(d))
+print(len(d.strip()))
+
+#print(b.split(","))
+#print(c.splitlines())
+#print(type(a))
+#print(a.upper())
+#print(a.lower())
+#print(a.capitalize())
+#print(a.title())
+#print(a.count("m"))
+#print(a.find("m"))
+#print(a.isupper())
+#print(a.find("m",7))
+#print(a.find("m",4))
+#print(a.find("k",5))
